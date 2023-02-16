24f98fa4eff9715d870e3bc2249888a4dd75fb0c
esperalda <oluwapemimary@gmail.com>
2023-02-16 13:17:08 -0800

    100-intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+printf("Size of a char: zu byte(s)\n", sizeof(char))
+	printf("size of an int: zu byte(s)\n", sizeof(char));
+printf("sizeof a lon int: 
