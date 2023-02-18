54b276efdd21255d0ae9e658c6d366d7058937bd
esperalda <oluwapemimary@gmail.com>
2023-02-18 01:39:29 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,10 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
+printf("Size of char: %zu byte(s)\n" , sizeof(char));
+printf("Size of int: %zu byte(s)\n" , sizeof(int));
+printf("Size of a long int: %zu byte(s)\n" , sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n" , sizeof(long long int));
+printf("Size of float: %zu byte(s)\n" , sizeof(float));
 return (0);
 }
