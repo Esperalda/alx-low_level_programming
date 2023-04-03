49ad45d6f59b7e90272a70769adcf303356f4f5d
Esperalda <oluwapemimary@gmail.com>
2023-04-03 11:54:01 -0700

    Linked List Updates

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -12,10 +12,12 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *ptr = malloc(sizeof(listint_t));
-ptr->data = n;
-ptr->link = 0;
 
-ptr->link = *head;
+if (ptr == 0);
+return (0);
+
+ptr->n = n;
+ptr->next= *head;
 *head = ptr;
-return (head);
+return (ptr);
 }
