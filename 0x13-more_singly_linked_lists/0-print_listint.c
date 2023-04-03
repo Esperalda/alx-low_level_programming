0308ce540b772ca3b7c9992cb26d27ec1a7f46ef
Esperalda <oluwapemimary@gmail.com>
2023-04-03 10:34:46 -0700

    Linked List Updates

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stddef.h>
+
+/**
+* print_listint - prints a singly linked list
+* @h: pointer to head of singly linked list
+*
+* Return: Number of elements in list
+*/
+size_t print_listint(const listint_t *h)
+{
+int count = 1;
+
+while (h != NULL)
+{
+printf("%i\n", h->n);
+h = h->next;
+count += 1;
+}
+return (count);
+}
