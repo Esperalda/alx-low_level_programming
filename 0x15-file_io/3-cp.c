0b03d3b1f9542b220373958e846f5fdcfa96e7f2
Esperalda <oluwapemimary@gmail.com>
2023-04-10 13:31:32 -0700

    File I_O Updates

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,49 +1,53 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
 /**
-* main - the program that copies the content of a file to another file
+* main - A program that copies the content of a file to another file
 *
-* @argc: it Counts the number of parameters that go into main
-* @argv: It is the Pointer of array of pointers containing strings entering main
+* @argc: It Counts the number of parameters that go into main
+* @argv: Pointer of array of pointers containing strings entering main
 * Return: Always 0 on (Success)
 *
 * if the number of argument is not the correct one, exit with code 97
 * and print Usage: cp file_from file_to, followed by a new line,
 * on the POSIX standard error
 *
- * if file_from does not exist, or if you can not read it, exit with
- * code 98 and print Error: Can't read from file NAME_OF_THE_FILE,
- * followed by a new line, on the POSIX standard error
- *
- * if you can not close a file descriptor ,
- * exit with code 100 and print Error:
- * Can't close fd FD_VALUE, followed by a new line,
- * on the POSIX standard error
- */
-
+* if file_from does not exist, or if you can not read it, exit with
+* code 98 and print Error: Can't read from file NAME_OF_THE_FILE,
+* followed by a new line, on the POSIX standard error
+*
+* if you can not close a file descriptor ,
+* exit with code 100 and print Error:
+* Can't close fd FD_VALUE, followed by a new line,
+* on the POSIX standard error
+*/
 int main(int argc, char **argv)
 {
-int fdfrom, fdto, checkr, checkw, checkc1, checkc2;
+int wdfrom, wdto, checkr, checkw, checkc1, checkc2;
 char buff[1024];
 
 if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-fdfrom = open(argv[1], O_RDONLY);
-if (fdfrom == -1)
+
+wdfrom = open(argv[1], O_RDONLY);
+if (wdfrom == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 
-fdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (fdto == -1)
+wdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (wdto == -1)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 
-while ((checkr = read(fdfrom, buff, 1024)) > 0)
+while ((checkr = read(wdfrom, buff, 1024)) > 0)
 {
-checkw = write(fdto, buff, checkr);
+checkw = write(wdto, buff, checkr);
 if (checkw != checkr)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -55,12 +59,12 @@ if (checkr == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-checkc1 = close(fdfrom);
+checkc1 = close(wdfrom);
 if (checkc1 == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdfrom), exit(100);
-checkc2 = close(fdto);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", wdfrom), exit(100);
+checkc2 = close(wdto);
 if (checkc2 == -1)
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdto), exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", wdto), exit(100);
 
 return (0);
 }
