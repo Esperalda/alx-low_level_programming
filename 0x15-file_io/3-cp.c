66ec7be0799b153e98a62f11de57fc5fadaead95
Esperalda <oluwapemimary@gmail.com>
2023-04-10 12:51:54 -0700

    File I_O Updates

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,17 +25,21 @@ int main(int argc, char **argv)
 {
 int wdfrom, wdto, checkin, checkout, checkup, checkdown;
 char buff[1024];
+
 if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+
 wdfrom = open(argv[1], O_RDONLY);
 if (wdfrom == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
+
 wdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (wdto == -1)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+
 while ((checkin = read(wdfrom, buff, 1024)) > 0)
 {
 checkout = write(wdto, buff, checkin);
@@ -56,5 +60,6 @@ dprintf(STDERR_FILENO, "Error: Can't close wd %d\n", wdfrom), exit(100);
 checkdown = close(wdto);
 if (checkdown == -1)
 dprintf(STDERR_FILENO, "Error: Can't close wd %d\n", wdto), exit(100);
+
 return (0);
 }
