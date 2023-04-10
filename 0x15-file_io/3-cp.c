1a49e801df33ef36b6828c2876eaef9d232210a5
Esperalda <oluwapemimary@gmail.com>
2023-04-10 13:13:18 -0700

    File I_O Updates

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -21,45 +21,46 @@
  * Can't close fd FD_VALUE, followed by a new line,
  * on the POSIX standard error
  */
+
 int main(int argc, char **argv)
 {
-int wdfrom, wdto, checkin, checkout, checkup, checkdown;
+int fdfrom, fdto, checkr, checkw, checkc1, checkc2;
 char buff[1024];
 
 if (argc != 3)
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-
-wdfrom = open(argv[1], O_RDONLY);
-if (wdfrom == -1)
+fdfrom = open(argv[1], O_RDONLY);
+if (fdfrom == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 
-wdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-if (wdto == -1)
+fdto = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+if (fdto == -1)
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
-while ((checkin = read(wdfrom, buff, 1024)) > 0)
+
+while ((checkr = read(fdfrom, buff, 1024)) > 0)
 {
-checkout = write(wdto, buff, checkin);
-if (checkout != checkin)
+checkw = write(fdto, buff, checkr);
+if (checkw != checkr)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 }
-if (checkin == -1)
+if (checkr == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-checkup = close(wdfrom);
-if (checkup == -1)
-dprintf(STDERR_FILENO, "Error: Can't close wd %d\n", wdfrom), exit(100);
-checkdown = close(wdto);
-if (checkdown == -1)
-dprintf(STDERR_FILENO, "Error: Can't close wd %d\n", wdto), exit(100);
+checkc1 = close(fdfrom);
+if (checkc1 == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdfrom), exit(100);
+checkc2 = close(fdto);
+if (checkc2 == -1)
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fdto), exit(100);
 
 return (0);
 }
