f548f635a05365c4e04a21cef610f52d152d4c75
Esperalda <oluwapemimary@gmail.com>
2023-04-10 09:46:26 -0700

    File I_O Updates

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+* read_textfile -  reads a text file and prints it
+* to the POSIX standard output
+* @filename: name of the file
+* @letters: number of letters it should read and print
+*
+* Return: returns the actual number of letters it could read and print
+* 0 if file cannot be opened or read
+* 0 if filename is NULL
+* 0 if write fails or does not write expected amount of bytes
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int wd, checkout, checkin;
+char *c;
+
+if (filename == NULL)
+return (0);
+
+c = malloc(letters + 1);
+
+if (c == 0)
+return (0);
+
+wd  = open(filename, O_RDONLY);
+
+if (wd == -1)
+return (free(c), 0);
+
+checkout = read(wd, c, letters);
+
+if (checkout == -1)
+return (free(c), 0);
+
+c[letters] = '\0';
+
+checkin = write(STDOUT_FILENO, c, checkout);
+if (checkin == -1)
+return (free(c), 0);
+
+free(c);
+close(fd);
+return (checkin);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,11 @@
+This Project, demonstrates how to create, Open a text file for both reading and writing. It first truncates the file to zero length if it exists, otherwise creates a file if it does not exist.
+
+A file represents a sequence of bytes, regardless of it being a text file or a binary file. C programming language provides access on high level functions as well as low level (OS level) calls to handle file on  storage devices.
+
+Below are examples to be executed in this project:
+
+0. Tread lightly, she is near: To write a function that reads a text file and prints it to the POSIX standard output.
+1. Under the snow: To create a function that creates a file.
+2. Speak gently, she can hear: To write a function that appends text at the end of a file.
+3. cp: To write a program that copies the content of a file to another file.
+4. elf: To write a program that displays the information contained in the ELF header at the start of an ELF file.
